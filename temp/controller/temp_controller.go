fc9966dac1a4209c2e8ae3188f0e8d535e4fb80b
Swayamshree <mohantyswayamshree@gmail.com>
2022-12-13 11:42:29 +0530

    code reverted

--- temp/controller/temp_controller.go
+++ temp/controller/temp_controller.go
@@ -102,7 +102,6 @@ func(tempController *TempController) broadCastTempDataInWebSocket()(error) {
 }
 
 func (tempController *TempController)WebSocketHandler(w http.ResponseWriter, r *http.Request) {
-	http.ListenAndServe(":80",nil)
     upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
 	//upgrade this connection to a WebSocket
